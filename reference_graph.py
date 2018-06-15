0c902111ee2f60b6eaf1d6a7bc045c39aafccd3e
Michal Staniaszek <m.staniaszek@gmail.com>
2018-06-15 08:04:16 +0200

    Attempting to improve similarity stuff, with tests
    Function to use punctuation in the string to get some hypothesis about a good
    separation of the authors and title and journal, works passably
    
    Separate function for checking if the stopped arrays for two references contain the same reference

--- reference_graph.py
+++ reference_graph.py
@@ -2,13 +2,24 @@
 import sys
 import os
 import logging
+import string
 import pickle
 from extract_references import ReferenceGroup, refgroup_from_filelist, FilenameRegex
+import unidecode
 import pygraphviz as pgv
 from graph_tool.all import *
 
-stopwords = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than', 'convolutional', 'neural', 'networks', 'semantic', 'segmentation']
-punctuation = "-,.():;'"
+# Most usual stopwords would be quite unique if they appeared in a title, we are
+# only interested in those which are really common or uninformative
+stopwords = ['an', 'for', 'do', 'its', 'of', 'is', 'or', 'who', 'from', 'the', 'are', 'to', 'at', 'and', 'in', 'on', 'a', 'by']
+# Some terms appear a lot in papers, might want to filter them
+term_stopwords = ['convolutional', 'neural', 'networks', 'semantic', 'segmentation', 'deep']
+
+# Strings which often come in the journal section of a reference
+conf_strings = ["proceedings", "conference", "journal", "transactions", "letters", "advances", "arxiv", " conf.", " proc.", "ieee", "international", "int.", "volume", "eccv", "icra", "cvpr", "iccv", " acm "]
+
+
+punctuation = ",.():;'\""
 
 def match_reference_to_title(docs):
     logger = logging.getLogger()
@@ -18,7 +29,7 @@ def match_reference_to_title(docs):
     graph.add_vertex(len(docs))
 
     doc_sets = {d.short_name(): {} for d in docs}
-    
+
     for from_ind, d in enumerate(docs):
         logger.debug("--------------------")
         logger.debug("checking {}, {}".format(d.author, d.title))
@@ -49,7 +60,7 @@ def match_reference_to_title(docs):
                 else:
                     ref_set = doc_sets[d.short_name()]["ref_sets"][ref_ind]
                     ref_trimmed = doc_sets[d.short_name()]["ref_trims"][ref_ind]
-                
+
                 # lowercase string due to possible different capitalisation schemes, remove certain punctuation
                 intersect = ref_set & d2title_set
                 if len(intersect) > 5:
@@ -62,11 +73,8 @@ def match_reference_to_title(docs):
                     #logger.debug("Adding edge between {} and {}".format(d.short_name(), d2.short_name()))
                     edges.append((d.short_name(), d2.short_name()))
                     graph.add_edge(graph.vertex(from_ind),graph.vertex(to_ind))
-                    
-    #graph_tool
-    pos = sfdp_layout(graph)
-    graph_draw(graph, pos=pos, output="sfdp.pdf")
 
+    #graph tool
     arf = arf_layout(graph, d=3, max_iter=5000)
     graph_draw(graph, pos=arf, output="arf.pdf")
 
@@ -78,43 +86,55 @@ def match_reference_to_title(docs):
     #     print(node, graph.in_degree(node))
 
     # graph.draw("refs.pdf", prog="neato", args="-Goverlap=false")
-        
+
 #    logger.debug(sorted(graph.in_degree(with_labels=True).items(), key=operator.itemgetter(1)))
 
-def similar_references(docs):
-    conf_strings = ["proceedings", "conference", "journal", "transactions", "letters", "advances", "arxiv", " conf.", " proc.", "ieee", "international", "int."]
+def similar_references(docs, overlap_prop = 0.6, min_set_size = 4):
     # If the intersection of the two sets is greater than this proportion of the
     # length of the shorter set, the two references are considered similar
-    overlap_prop = 0.5
-    min_set_size = 5
-    
+
     all_refs = []
+
     for d in docs:
         for r in d.references:
             stripped = r.lower().strip()
+            stripped = str(unidecode.unidecode(unicode(stripped, 'utf8')))
+
+            sep_inds, sep_inds_rev = punctuation_density_separate(stripped)
+            if not sep_inds:
+                continue
+
+            punc_sep = stripped[sep_inds[0]:sep_inds_rev[-1]].strip()
+
             conf_inds = []
             for conf_str in conf_strings:
-                found_ind = stripped.find(conf_str)
+                found_ind = punc_sep.find(conf_str)
+                # print("found conf string {} at ind {}".format(conf_str, found_ind))
                 if found_ind > 0:
                     conf_inds.append(found_ind)
 
+            print(conf_inds)
             if not conf_inds:
                 # print("found no conference strings")
-                conf_strip = stripped
+                conf_strip = punc_sep
             else:
                 str_start = min(conf_inds)
-                last_dot = stripped.rfind(".", 0, str_start)
-                last_comma = stripped.rfind(",", 0, str_start)
+                last_dot = punc_sep.rfind(".", 0, str_start)
+                last_comma = punc_sep.rfind(",", 0, str_start)
 
                 # print("conf string started at {}, last dot at {}, last comma at {}".format(str_start, last_dot, last_comma))
-                # print("all after latest punctuation: {}".format(stripped[max(last_dot, last_comma):]))
-                conf_strip = stripped[:max(last_dot, last_comma)]
+                # print("all after latest punctuation: {}".format(punc_sep[max(last_dot, last_comma):]))
+                conf_strip = punc_sep[:max(last_dot, last_comma)]
 
-            
-            ref_trimmed = conf_strip.lower().translate(None, punctuation)
+            full_strip = conf_strip
+
+            # print("conf_strip: {}\nfull strip: {}".format(conf_strip, full_strip))
+
+            ref_trimmed = full_strip.lower().translate(None, punctuation)
             ref_stopped = [word for word in ref_trimmed.split(" ") if word not in stopwords and len(word) > 2]
             ref_set = set(ref_stopped)
-            all_refs.append((r, ref_stopped, ref_set, d.short_name()))
+            all_refs.append((r.strip(), ref_stopped, ref_set, d.short_name(), punc_sep, full_strip))
+
 
 
     # List for each reference of references which are similar
@@ -122,7 +142,7 @@ def similar_references(docs):
     for cur_ind, current in enumerate(all_refs):
         print("{}/{}".format(cur_ind, len(all_refs)))
         print("--------------------------------------------------")
-
+        print(all_refs[cur_ind][0])
         cur_setlen = len(current[2])
         for other_ind, other in enumerate(all_refs):
             other_setlen = len(other[2])
@@ -130,22 +150,64 @@ def similar_references(docs):
             if cur_ind >= other_ind:
                 continue
 
-            if cur_setlen < min_set_size or other_setlen < min_set_size:
+            if cur_setlen <= min_set_size or other_setlen <= min_set_size:
                 continue
 
             max_count = min(cur_setlen, other_setlen)
             common = current[2].intersection(other[2])
             overlap = len(common)/float(max_count)
+
             if overlap > overlap_prop:
-                similar[cur_ind].append(other_ind)
-                similar[other_ind].append(cur_ind)
-                # print("Overlap {} exceeds overlap proportion {}".format(overlap, overlap_prop))
-                # print("{}\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n{}".format(current[0], other[0]))
-                # print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-                # print("{}\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n{}".format(current[1], other[1]))
-                # print("==================================================")
+                # Look at the stopped lists of words extracted from the
+                # reference. This is quite a strict measure, doesn't consider
+                # possibility of stuff after the title being included
+                c_list = all_refs[cur_ind][1]
+                o_list = all_refs[other_ind][1]
+                if c_list == o_list or arrays_contain_same_reference(c_list, o_list, common):
+                    similar[cur_ind].append(other_ind)
+                    similar[other_ind].append(cur_ind)
+
 
     return all_refs, similar
+
+def arrays_contain_same_reference(first, second, common):
+    print("\n--------------------------------------------------")
+    print("Arrays not equal:")
+    print(first)
+    print(second)
+
+    # Find the index of occurrences of common words in the two references,
+    # and use that information to get approximate extents of the title. If
+    # the title is the same, then the length of the list from earliest to
+    # latest occurrence of common words should be the same for both of the
+    # lists given
+    first_inds = []
+    second_inds = []
+    for word in common:
+        first_inds.append(first.index(word))
+        second_inds.append(second.index(word))
+
+    print("inds common first: {}".format(sorted(first_inds)))
+    print("inds common second: {}".format(sorted(second_inds)))
+
+    first_min = min(first_inds)
+    first_max = max(first_inds)
+    second_min = min(second_inds)
+    second_max = max(second_inds)
+
+    extent_first = first[first_min:first_max + 1]
+    extent_second = second[second_min:second_max + 1]
+    print("common extent list first: {}".format(extent_first))
+    print("common extent list second: {}".format(extent_second))
+
+    if extent_first == extent_second:
+        return True
+    
+    # need to maybe consider more here, because there may be repeated
+    # common words in one of the references, e.g. perhaps the name of
+    # the journal has one of the words in it and hasn't been stripped
+    # correctly.
+    return False
 
 def process_similar(all_refs, similar):
     groups = []
@@ -153,7 +215,8 @@ def process_similar(all_refs, similar):
     for ind, l in enumerate(similar):
         if ind in processed:
             continue
-
+        print("--------------------------------------------------")
+        print(all_refs[ind][0])
         group = []
         to_process = [ind]
         while to_process:
@@ -163,18 +226,31 @@ def process_similar(all_refs, similar):
             check_ind = to_process.pop(0)
             group.append(check_ind)
             new_group = similar[check_ind]
-#            print("checking new group at index {}: {}".format(check_ind, new_group))
+            increase = len(new_group)/len(group)
+            tp = set(to_process)
+            ng = set(new_group)
+            overlap = tp.intersection(ng)
+            diff = tp.difference(ng)
+            print("overlapping elements: {}, new elements: {}".format(len(overlap), len(diff)))
+            print("new additions:")
+            for i in diff:
+                print("{}".format(all_refs[i][0]))
+
+            group.extend(new_group)
             for elem in new_group:
                 if elem not in to_process and elem not in group:
-                    to_process.append(elem)
+                    #to_process.append(elem)
+                    pass
                     #print("element {} not in group, adding to to_process".format(elem))
                 else:
                     pass
                     #print("element {} was already processed, or in the list to be processed.".format(elem))
+        print("final group size {}".format(len(group)))
 
         groups.append(group)
         processed.extend(group)
 
+    groups = sorted(groups, key=lambda x: len(x))
     for group in groups:
         print("--------------------------------------------------")
         print("group size: {}".format(len(group)))
@@ -183,15 +259,64 @@ def process_similar(all_refs, similar):
 
     print("Number of groups: {}".format(len(groups)))
 
+
+
     # for ind, similar_arr in enumerate(similar):
     #     print("----------------------------------------")
     #     print(all_refs[ind][0])
 
     #     for sml in similar_arr:
     #         print("Cited in {}: {}".format(all_refs[sml][3], all_refs[sml][0]))
-        
+
     #     if ind > 10:
     #         break
+
+def punctuation_density_separate(ref, sep_thresh=20):
+    punctuation_inds = [ind for ind, char in enumerate(ref) if char in ',.']
+    sep_inds = []
+    sep_inds_reverse = []
+
+    # Going forwards (with sep_inds) gives the punctuation furthest along the
+    # string which conforms to the separation threshold. Use this to track the
+    # start of the sequence and add it to the reverse indices, so that it gives
+    # the start of the sequence
+    rev_ind = None
+    for ind, p_ind in enumerate(punctuation_inds):
+        # At the end of the array just insert the currently tracked indices
+        if ind == len(punctuation_inds) - 1:
+            sep_inds.append(p_ind)
+            sep_inds_reverse.append(rev_ind if rev_ind else p_ind)
+            break
+
+        # If the punctuation distance is within the threshold, then p_ind is the
+        # start of a sequence
+        if abs(punctuation_inds[ind+1] - p_ind) < sep_thresh:
+            if not rev_ind:
+                rev_ind = p_ind
+            continue
+        else:
+            # End of the sequence
+            # +1 so we don't include the actual punctuation
+            sep_inds.append(p_ind + 1)
+            sep_inds_reverse.append(rev_ind if rev_ind else p_ind)
+
+            rev_ind = None
+
+    # print("--------------------------------------------------")
+    # print(ref)
+    # print("forwards: {}".format(sep_inds))
+    # print("reverse: {}".format(sep_inds_reverse))
+    # for ind, sep_ind in enumerate(sep_inds_reverse):
+    #     if ind == 0:
+    #         sep_str = ref[:sep_ind]
+    #     else:
+    #         # +1 so that we don't include the punctuation
+    #         sep_str = ref[sep_inds[ind-1]:sep_ind+1]
+
+    if sep_inds:
+        print(ref[sep_inds[0]:sep_inds_reverse[-1]])
+
+    return sep_inds, sep_inds_reverse
 
 def main():
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(message)s')
@@ -212,9 +337,17 @@ def main():
             all_refs = pickle.load(f)
         with open("similar.pickle", 'r') as f:
             similar = pickle.load(f)
-    
+
+    with open("all_refs_fullstrip.txt", 'w') as f:
+        for r in all_refs:
+            f.write("{}\n".format(r[5]))
+
+    with open("all_refs_stopped.txt", 'w') as f:
+        for r in all_refs:
+            f.write("{}\n".format(r[1]))
+
+
     process_similar(all_refs, similar)
 
 if __name__ == '__main__':
     main()
-    
---
+++ reference_graph_test.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+
+import random
+import unittest
+from reference_graph import arrays_contain_same_reference
+
+paper_title_similar_pairs = [(['semantic', 'image', 'segmentation', 'with', 'deep', 'convolutional', 'nets', 'fully', 'connected', 'crfs'],
+                              ['deeplab', 'semantic', 'image', 'segmentation', 'with', 'deep', 'convolutional', 'nets', 'atrous', 'convolution', 'fully', 'connected', 'crfs']),
+                             (['pascal', 'visual', 'object', 'classes', 'voc', 'challenge'],
+                              ['pascal', 'visual', 'object', 'classes', 'challenge', '2012', 'voc2012', 'results'])]
+paper_titles = [['whats', 'point', 'semantic', 'segmentation', 'with', 'point', 'supervision'],
+                ['convolutional', 'random', 'walk', 'networks', 'semantic', 'image', 'segmentation'],
+                ['fully', 'convolutional', 'networks', 'semantic', 'segmentation'],
+                ['deeplab', 'semantic', 'image', 'segmentation', 'with', 'deep', 'convolutional', 'nets', 'atrous', 'convolution', 'fully', 'connected', 'crfs'],
+                ['boxsup', 'exploiting', 'bounding', 'boxes', 'supervise', 'convolutional', 'networks', 'semantic', 'segmentation']]
+
+junk = [['geoffrey', 'hinton', '2012'],
+        ['trevor', 'darrell'],
+        ['anton', 'van', 'den', 'hengel', 'ian', 'reid'],
+        ['torch7', 'matlab-like', 'environment', 'machine', 'learning', 'biglearn', 'nips', 'workshop', 'dai', 'sun', '2015'],
+        ['volume', 'jmlr', 'proceedings', 'pages', '195-206', 'jmlrorg', '2012', 'karen', 'simonyan', 'andrew', 'zisserman'],
+        ['url', 'http', '//wwwpascal-networkorg/challenges/', 'voc/voc2012/workshop/index'],
+        ['pereira', 'burges', 'cjc', 'bottou', 'weinberger', 'eds'],
+        ['pratikakis', 'dupont', 'ovsjanikov', 'editors', 'eurographics', 'workshop', 'object', 'retrieval', 'eurographics', 'association'],
+        ['lafferty', 'williams', 'cki', 'shawe-taylor', 'zemel', 'culotta', 'eds'],
+        ['navab', 'hornegger', 'wells', 'frangi', 'eds', 'medical', 'image', 'computing', 'computer-assisted', 'intervention', 'miccai', '2015'],
+        ['3rd', 'workshop', 'semantic', 'perception', 'mapping', 'exploration', 'spme'],
+        ['isprs', 'annals', 'photogrammetry', 'remote', 'sensing', 'spatial', 'information', 'sciences']]
+
+class TestArraySimilarityMethods(unittest.TestCase):
+
+
+    
+    def test_noclutter(self):
+        for ind, title in enumerate(paper_titles):
+            common = set(title).intersection(set(title))
+            self.assertTrue(arrays_contain_same_reference(title, title, common))
+
+    def test_noclutter_invalid(self):
+        first = paper_titles[0]
+        second = paper_titles[1]
+        common = set(first).intersection(set(second))
+
+        self.assertFalse(arrays_contain_same_reference(first, second, common))
+
+    def test_noclutter_similar(self):
+        similar_ind = random.randint(0, len(paper_title_similar_pairs) - 1)
+        
+        first = paper_title_similar_pairs[similar_ind][0]
+        second = paper_title_similar_pairs[similar_ind][1]
+        common = set(first).intersection(set(second))
+
+        self.assertFalse(arrays_contain_same_reference(first, second, common))
+
+    def test_noclutter_cutoff(self):
+        # What do we want to be the case here? If we remove the last word in one
+        # of the titles then it's sort of like the other one has junk at the
+        # end?
+        first = paper_titles[0]
+        second = paper_titles[1][:-1]
+        common = set(first).intersection(set(second))
+
+        # Go with a more inclusive assumption for now...
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+    def test_one_clutter_before(self):
+        paper_ind = random.randint(0, len(paper_titles) - 1)
+        junk_ind = random.randint(0, len(junk) - 1)
+        
+        first = paper_titles[paper_ind]
+        second = junk[junk_ind] + paper_titles[paper_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+        # check it works if they are the other way round
+        first = junk[junk_ind] + paper_titles[paper_ind]
+        second = paper_titles[paper_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+    def test_one_clutter_before_invalid(self):
+        paper_ind = random.randint(0, len(paper_titles) - 1)
+        paper_ind_different = paper_ind - 1 if paper_ind > 0 else paper_ind + 1
+        junk_ind = random.randint(0, len(junk) - 1)
+        
+        first = paper_titles[paper_ind]
+        second = junk[junk_ind] + paper_titles[paper_ind_different]
+        common = set(first).intersection(set(second))
+
+        self.assertFalse(arrays_contain_same_reference(first, second, common))
+
+        # check it works if they are the other way round
+        first = junk[junk_ind] + paper_titles[paper_ind_different]
+        second = paper_titles[paper_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertFalse(arrays_contain_same_reference(first, second, common))
+
+    def test_one_clutter_before_similar(self):
+        similar_ind = random.randint(0, len(paper_title_similar_pairs) - 1)
+        junk_ind = random.randint(0, len(junk) - 1)
+        
+        first = paper_title_similar_pairs[similar_ind][0]
+        second = junk[junk_ind] + paper_title_similar_pairs[similar_ind][1]
+        common = set(first).intersection(set(second))
+
+        self.assertFalse(arrays_contain_same_reference(first, second, common))
+
+    def test_one_clutter_after(self):
+        paper_ind = random.randint(0, len(paper_titles) - 1)
+        junk_ind = random.randint(0, len(junk) - 1)
+        
+        first = paper_titles[paper_ind]
+        second = paper_titles[paper_ind] + junk[junk_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+        # First paper has junk rather than second
+        first = paper_titles[paper_ind] + junk[junk_ind]
+        second = paper_titles[paper_ind] 
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+    def test_one_clutter_after_similar(self):
+        similar_ind = random.randint(0, len(paper_title_similar_pairs) - 1)
+        junk_ind = random.randint(0, len(junk) - 1)
+        
+        first = paper_title_similar_pairs[similar_ind][0]
+        second = paper_title_similar_pairs[similar_ind][1] + junk[junk_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertFalse(arrays_contain_same_reference(first, second, common))
+    
+    def test_one_clutter_after_repeatword(self):
+        # use clutter which contains a word which exists in the paper title
+        paper = ['whats', 'point', 'semantic', 'segmentation', 'with', 'point', 'supervision']
+        junk = ['3rd', 'workshop', 'semantic', 'perception', 'mapping', 'exploration', 'spme']
+        
+        first = paper
+        second = paper + junk
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+        first = paper + junk
+        second = paper
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+    def test_one_clutter_before_repeatword(self):
+        paper = ['deeplab', 'semantic', 'image', 'segmentation', 'with', 'deep', 'convolutional', 'nets', 'atrous', 'convolution', 'fully', 'connected', 'crfs']
+        junk = ['navab', 'hornegger', 'wells', 'frangi', 'eds', 'medical', 'image', 'computing', 'computer-assisted', 'intervention', 'miccai', '2015']
+
+        first = paper
+        second = junk + paper
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+    def test_one_clutter_before_after_repeatword_before(self):
+        paper = ['deeplab', 'semantic', 'image', 'segmentation', 'with', 'deep', 'convolutional', 'nets', 'atrous', 'convolution', 'fully', 'connected', 'crfs']
+        junk_before = ['navab', 'hornegger', 'wells', 'frangi', 'eds', 'medical', 'image', 'computing', 'computer-assisted', 'intervention', 'miccai', '2015']
+        junk_after = ['volume', 'jmlr', 'proceedings', 'pages', '195-206', 'jmlrorg', '2012', 'karen', 'simonyan', 'andrew', 'zisserman']
+
+        first = paper
+        second = junk_before + paper + junk_after
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+    def test_one_clutter_before_after_repeatword_after(self):
+        paper = ['deeplab', 'semantic', 'image', 'segmentation', 'with', 'deep', 'convolutional', 'nets', 'atrous', 'convolution', 'fully', 'connected', 'crfs']
+        junk_before = ['torch7', 'matlab-like', 'environment', 'machine', 'learning', 'biglearn', 'nips', 'workshop', 'dai', 'sun', '2015']
+        junk_after = ['3rd', 'workshop', 'semantic', 'perception', 'mapping', 'exploration', 'spme']
+
+        first = paper
+        second = junk_before + paper + junk_after
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+        
+    def test_one_clutter_before_after(self):
+        paper_ind = random.randint(0, len(paper_titles) - 1)
+        junk_before_ind = random.randint(0, len(junk) - 1)
+        junk_after_ind = random.randint(0, len(junk) - 1)
+        
+        while junk_before_ind == junk_after_ind:
+            junk_after_ind = random.randint(0, len(junk) - 1)
+        
+        first = paper_titles[paper_ind]
+        second = junk[junk_before_ind] + paper_titles[paper_ind] + junk[junk_after_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+        # First paper has junk rather than second
+        first = junk[junk_before_ind] + paper_titles[paper_ind] + junk[junk_after_ind]
+        second = paper_titles[paper_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+    def test_both_clutter_before(self):
+        paper_ind = random.randint(0, len(paper_titles) - 1)
+        junk_first_ind = random.randint(0, len(junk) - 1)
+        junk_second_ind = random.randint(0, len(junk) - 1)
+        
+        while junk_first_ind == junk_second_ind:
+            junk_second_ind = random.randint(0, len(junk) - 1)
+        
+        first = junk[junk_first_ind] + paper_titles[paper_ind]
+        second = junk[junk_second_ind] + paper_titles[paper_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+    def test_both_clutter_after(self):
+        paper_ind = random.randint(0, len(paper_titles) - 1)
+        junk_first_ind = random.randint(0, len(junk) - 1)
+        junk_second_ind = random.randint(0, len(junk) - 1)
+        
+        while junk_first_ind == junk_second_ind:
+            junk_second_ind = random.randint(0, len(junk) - 1)
+        
+        first = paper_titles[paper_ind] + junk[junk_first_ind]
+        second = paper_titles[paper_ind] + junk[junk_second_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+    def test_both_clutter_before_after(self):
+        paper_ind = random.randint(0, len(paper_titles) - 1)
+        junk_before_first_ind = random.randint(0, len(junk) - 1)
+        junk_before_second_ind = random.randint(0, len(junk) - 1)
+        junk_after_first_ind = random.randint(0, len(junk) - 1)
+        junk_after_second_ind = random.randint(0, len(junk) - 1)
+        
+        while junk_before_first_ind == junk_before_second_ind:
+            junk_before_second_ind = random.randint(0, len(junk) - 1)
+
+        while junk_after_first_ind == junk_after_second_ind:
+            junk_after_second_ind = random.randint(0, len(junk) - 1)
+
+        
+        first = junk[junk_before_first_ind] + paper_titles[paper_ind] + junk[junk_after_first_ind]
+        second = junk[junk_before_second_ind] + paper_titles[paper_ind] + junk[junk_after_second_ind]
+        common = set(first).intersection(set(second))
+
+        self.assertTrue(arrays_contain_same_reference(first, second, common))
+
+if __name__ == '__main__':
+    unittest.main()
